93b0fd7dde62ee89d0de7247d8f8e222a9013aa7
Agustin <143128561+Agustinsote03@users.noreply.github.com>
2026-04-04 17:15:43 -0300

    Se quita la función de prueba y se añade la función 'Procesar texto' para devolver las estadisticas de un texto

--- src/funciones.py
+++ src/funciones.py
@@ -1,2 +1,19 @@
-def funcion1():
-    print("Esta es la función 1")
+def procesar_texto(texto):
+    """
+    La función recibe el texto contenido en el string, lo separa en saltos de linea que guarda en la lista 'lista_lineas' y por cada elementos de la
+    lista se itera contando las palabras y sumandolas a la variable acumuladora 'cant_palabras'.
+    """
+
+ #Separamos el texto por saltos de linea, se obtiene una lista donde cada elemento es un string(linea)
+    lista_lineas = texto.split("\n")
+
+    cant_lineas = len(lista_lineas)
+    cant_palabras = 0
+
+    for linea in lista_lineas:
+        palabras = linea.split()
+        cant_palabras += len(palabras)
+    prom_palabras = cant_palabras / cant_lineas
+    
+    return {"Palabras por línea: " : prom_palabras, "Cantidad de líneas: " : cant_lineas, "Cantidad de palabras: " : cant_palabras}
+
