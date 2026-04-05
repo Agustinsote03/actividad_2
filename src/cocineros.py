02a6e0e50bf54c55647a9a321c189123689a2bc0
Agustin <143128561+Agustinsote03@users.noreply.github.com>
2026-04-05 06:17:40 -0300

    Se agrega linea de código con para hallar al ganador con max() y lambda; se incluye la actualización de estádisticas de cocineros por ronda

--- src/cocineros.py
+++ src/cocineros.py
@@ -6,8 +6,8 @@ def sumar_puntajes_jueces(puntaje_jueces):
     total_puntos = sum(puntaje_jueces.values())
     return total_puntos
 
-
-
+def generar_tabla_posiciones(estadisticas):
+    pass
 #------------------------------SIMULACIÓN DE COMPETENCIA----------------------------#
 def simular_competencia(rounds):
     """
@@ -28,10 +28,30 @@ def simular_competencia(rounds):
         scores_ronda = ronda['scores']
         tema = ronda['theme']
     
-    # Se procesa el puntaje (Con la forma {'Valentina': {'judge_1': 8, 'judge_2': 7,'judge_3': 9}') 
+        # Se procesa el puntaje (Con la forma {'Valentina': {'judge_1': 8, 'judge_2': 7,'judge_3': 9}') 
 
         resultados_ronda = list(map(lambda item: sumar_puntajes_jueces(item[1]), scores_ronda.items() ))
-        print(resultados_ronda)
+        ganador = max(resultados_ronda, key=lambda x: x[1])
+        nombre_ganador = ganador[0]
+        puntos_ganador = ganador[1]
+
+        #Se actualizan las estadísticas del ganador
+        estadisticas[nombre_ganador]['rondas_ganadas'] += 1
+
+        #Este bloque imprime la victoria
+        print(f"Ronda {ronda} - {tema}:")
+        print(f"  Ganador: {nombre_ganador} ({puntos_ganador} pts)")
+        print()
+
+        #Este bloque actualiza las estadisticas de cada cocinero
+        for nombre, puntos in resultados_ronda:
+
+            estadisticas[nombre]['total_puntos'] += puntos
+
+            if puntos > estadisticas[nombre]['mejor_ronda']:
+                estadisticas[nombre]['mejor_ronda'] = puntos
+        print()
+        generar_tabla_posiciones(estadisticas)
 
 
 
