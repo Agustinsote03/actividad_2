0c7a90c730b5574e94743fcecca3ad06b737ae4d
Agustin <143128561+Agustinsote03@users.noreply.github.com>
2026-04-05 06:01:04 -0300

    Se corrige errores de acceso en nombre_cocinero y error de iteración un error de tipeo definiendo 'estadisticas'. Se agrega 'resultados_ronda'

--- src/cocineros.py
+++ src/cocineros.py
@@ -1,13 +1,23 @@
+#------------------------------FUNCIONES AUXILIARES----------------------------#
+def sumar_puntajes_jueces(puntaje_jueces):
+    """
+     Esta función toma el diccionario con las puntuaciones de los jueces y retorna la suma de ellos
+    """
+    total_puntos = sum(puntaje_jueces.values())
+    return total_puntos
+
+
+
+#------------------------------SIMULACIÓN DE COMPETENCIA----------------------------#
 def simular_competencia(rounds):
     """
      simulacion
     """
 
-    #Se obtiene el nombre de los cocineros (las 'keys' del diccionario) y se guardan en la variable cocineros
-    nombre_cocinero = rounds['scores'].keys()
-
+    #De la ronda 0, se obtiene el nombre de los cocineros (las 'keys' del diccionario) y se guardan en la variable cocineros
+    nombre_cocinero = rounds[0]['scores'].keys()
     #Se define un diccionario por comprensión a partir de cocineros
-    estadisticas = {nombre_cocinero: {'total_puntos': 0, 'rondas_ganadas': 0, 'mejor_ronda': 0} for cocinero in nombre_cocinero}
+    estadisticas = {cocinero: {'total_puntos': 0, 'rondas_ganadas': 0, 'mejor_ronda': 0} for cocinero in nombre_cocinero}
 
     cant_rondas = len(rounds)
     ronda_actual = 1
@@ -17,8 +27,11 @@ def simular_competencia(rounds):
 
         scores_ronda = ronda['scores']
         tema = ronda['theme']
-        
-        resultados_ronda = 
+    
+    # Se procesa el puntaje (Con la forma {'Valentina': {'judge_1': 8, 'judge_2': 7,'judge_3': 9}') 
+
+        resultados_ronda = list(map(lambda item: sumar_puntajes_jueces(item[1]), scores_ronda.items() ))
+        print(resultados_ronda)
 
 
 
