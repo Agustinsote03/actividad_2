93d335af02eddcabd35351f0aee265a9d4282bc1
Agustin <143128561+Agustinsote03@users.noreply.github.com>
2026-04-05 05:02:18 -0300

    Se añade la función 'simular_competencia', se inicializan variables a partir de la estructura 'rounds'

--- src/cocineros.py
+++ src/cocineros.py
@@ -1,1 +1,24 @@
 def simular_competencia(rounds):
+    """
+     simulacion
+    """
+
+    #Se obtiene el nombre de los cocineros (las 'keys' del diccionario) y se guardan en la variable cocineros
+    nombre_cocinero = rounds['scores'].keys()
+
+    #Se define un diccionario por comprensión a partir de cocineros
+    estadisticas = {nombre_cocinero: {'total_puntos': 0, 'rondas_ganadas': 0, 'mejor_ronda': 0} for cocinero in nombre_cocinero}
+
+    cant_rondas = len(rounds)
+    ronda_actual = 1
+
+    #Se procesa cada ronda
+    for ronda in rounds:
+
+        scores_ronda = ronda['scores']
+        tema = ronda['theme']
+        
+        resultados_ronda = 
+
+
+
